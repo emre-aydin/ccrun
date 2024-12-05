6e3b7feb668b8a09e207ad8e119572c374ff15a0
emre <emre@aydin.tech>
2024-12-05 15:39:24 +0300

    Use the built binary for testing

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -6,35 +6,25 @@ import (
 	"math/rand/v2"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"testing"
 )
 
 func TestCommandLine(t *testing.T) {
-	binaryName := buildBinary(t)
-	assert.NotNil(t, binaryName)
+	binary := buildBinary(t)
+	assert.NotNil(t, binary)
 	defer func(name string) {
 		err := os.Remove(fmt.Sprintf("../%s", name))
 		if err != nil {
 			t.Log(err)
 		}
-	}(binaryName)
+	}(binary)
 
-	//command := exec.Command("./ccrun", "run", "echo", "Hello Coding Challenges!")
-	//command.Dir = "/home/emre/projects/ccrun"
-	//
-	//stdoutPipe, err := command.StdoutPipe()
-	//assert.Nil(t, err)
-	//
-	//err = command.Start()
-	//assert.Nil(t, err)
-	//
-	//stdoutMsg, err := io.ReadAll(stdoutPipe)
-	//assert.Nil(t, err)
-	//assert.Equal(t, "Hello Coding Challenges!\n", string(stdoutMsg))
-	//
-	//err = command.Wait()
-	//assert.Nil(t, err)
+	msg := "Hello Coding Challenges!"
+	stdout, stderr := executeBinary(t, binary, msg)
+	assert.Empty(t, stderr)
+	assert.Equal(t, stdout, fmt.Sprintf("%s\n", msg))
 }
 
 func buildBinary(t *testing.T) string {
@@ -47,6 +37,18 @@ func buildBinary(t *testing.T) string {
 	err := buildCmd.Run()
 	assert.Nil(t, err, "stdout:\n%s\nstderr:%s\n", stdout.String(), stderr.String())
 	return binary
+}
+
+func executeBinary(t *testing.T, binary, msg string) (string, string) {
+	dir, err := os.Getwd()
+	assert.Nil(t, err)
+	command := exec.Command(filepath.Join(dir, "..", binary), "run", "echo", msg)
+	var stdout, stderr strings.Builder
+	command.Stdout = &stdout
+	command.Stderr = &stderr
+	err = command.Run()
+	assert.Nil(t, err)
+	return stdout.String(), stderr.String()
 }
 
 func randomString(length int) string {
