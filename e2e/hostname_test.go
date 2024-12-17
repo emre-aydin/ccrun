aabc7294e3bada85eadb8e584d77945a72bb6f0a
Emre Aydin <emre@aydin.tech>
2024-12-17 09:31:22 +0300

    failing hostname_test

--- e2e/hostname_test.go
+++ e2e/hostname_test.go
@@ -1,1 +1,35 @@
-package e2e
+package e2e_test
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestHostname(t *testing.T) {
+	binary := buildBinary(t)
+	assert.NotNil(t, binary)
+	t.Cleanup(func() {
+		err := os.Remove(fmt.Sprintf("../%s", binary))
+		if err != nil {
+			t.Log(err)
+		}
+	})
+
+	originalHostname, err := os.Hostname()
+	assert.Nil(t, err)
+	assert.NotEmpty(t, originalHostname)
+
+	var stdout, stderr strings.Builder
+	cmd := exec.Command(binary, "run", "hostname")
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err = cmd.Run()
+	assert.Nil(t, err)
+	assert.NotEqual(t, originalHostname, strings.TrimSuffix(stdout.String(), "\n"))
+	assert.Empty(t, stderr.String())
+}
