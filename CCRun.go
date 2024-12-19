47f6cb09d11632320afb6ee142fbf4af375c74b1
Emre Aydin <emre@aydin.tech>
2024-12-19 10:34:11 +0300

    add wrap-run subcommand

--- CCRun.go
+++ CCRun.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
@@ -18,21 +19,45 @@ func main() {
 		fmt.Fprintln(os.Stderr, "no command to run")
 		os.Exit(1)
 	}
-	if args[0] == "run" {
+
+	switch args[0] {
+	case "run":
+		// replace 'run' with 'wrap-run' to start the new process in a new UTS namespace
+		args[0] = "wrap-run"
+		cmd := exec.Command(os.Args[0], args...)
+		cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Cloneflags: syscall.CLONE_NEWUTS,
+		}
+
+		err := cmd.Run()
+		if err != nil {
+			exitWithError(err)
+		}
+	case "wrap-run":
+		err := syscall.Sethostname([]byte("container"))
+		if err != nil {
+			exitWithError(err)
+		}
+
 		cmd := exec.Command(args[1], args[2:]...)
 		cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
-		err := cmd.Run()
-		var exitError *exec.ExitError
+		err = cmd.Run()
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			exitCode := 1
-			if errors.As(err, &exitError) {
-				exitCode = exitError.ExitCode()
-			}
-			os.Exit(exitCode)
+			exitWithError(err)
 		}
-	} else {
+	default:
 		fmt.Fprintf(os.Stderr, "invalid command: %s\n", args[0])
 		os.Exit(1)
 	}
+}
+
+func exitWithError(err error) {
+	var exitError *exec.ExitError
+	fmt.Fprintln(os.Stderr, err.Error())
+	exitCode := 1
+	if errors.As(err, &exitError) {
+		exitCode = exitError.ExitCode()
+	}
+	os.Exit(exitCode)
 }
