1798036f1d5b13ded149dc01b1a4072ca46c3f6a
Emre Aydin <emre@aydin.tech>
2024-12-19 18:31:26 +0300

    better hostname test

--- e2e/hostname_test.go
+++ e2e/hostname_test.go
@@ -18,17 +18,13 @@ func TestHostname(t *testing.T) {
 		}
 	})
 
-	originalHostname, err := os.Hostname()
-	assert.Nil(t, err)
-	assert.NotEmpty(t, originalHostname)
-
 	var stdout, stderr strings.Builder
 	cmd := exec.Command(binary, "run", "hostname")
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 
-	err = cmd.Run()
+	err := cmd.Run()
 	assert.Nil(t, err)
-	assert.NotEqual(t, originalHostname, strings.TrimSuffix(stdout.String(), "\n"))
+	assert.Equal(t, "container", strings.TrimSuffix(stdout.String(), "\n"))
 	assert.Empty(t, stderr.String())
 }
